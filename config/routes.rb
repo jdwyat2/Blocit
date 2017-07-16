1b535200bf09da2f9f9006fd81d08dc8611239e7
Jon <jdwyatt87@gmail.com>
2017-07-16 21:28:38 +0000

    Blockit_UserModel

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,9 @@ Rails.application.routes.draw do
     resources :posts, expect: [:index]
   end
   
+  resources :users, only: [:new, :create]
+  
   get 'about' => 'welcome#about'
 
- root 'posts#index'
+ root 'topics#index'
 end
