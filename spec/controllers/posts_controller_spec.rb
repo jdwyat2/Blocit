00a093a6f1cf9e7f4ed7b9f2eb6fe12557536f29
Jon <jdwyatt87@gmail.com>
2017-07-23 23:14:41 +0000

    Blockit_Authorization

--- spec/controllers/posts_controller_spec.rb
+++ spec/controllers/posts_controller_spec.rb
@@ -1,14 +1,17 @@
 require 'rails_helper'
 
+include RandomData
 include SessionsHelper
 
 RSpec.describe PostsController, type: :controller do
   
   let(:my_user) { User.create!(name: "Bloccit User", email: "user@bloccit.com", password: "helloworld") }
+  let(:other_user) { User.create!(name: RandomData.random_name, email: RandomData.random_email, password: "helloworld", role: :member) }
   let(:my_topic) {Topic.create!(name: RandomData.random_sentence, description: RandomData.random_paragraph)}
   let(:my_post){my_topic.posts.create!(title: RandomData.random_sentence, body: RandomData.random_paragraph, user: my_user)}
 
-  context "guest user" do
+  context "guest" do
+    
     describe "GET show" do
       
       it "returns http success" do
@@ -61,7 +64,7 @@ RSpec.describe PostsController, type: :controller do
     describe "DELETE destroy" do
       it "returns http redirect" do
         delete :destroy, topic_id: my_topic.id, id: my_post.id
-        expect(response).to have_http_status(:redirect)
+        expect(response).to redirect_to(new_session_path)
       end
     end
         
