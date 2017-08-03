43a6544e34efa1cdfd9e33d34c2b25b010e65a2c
Jon <jdwyatt87@gmail.com>
2017-08-03 00:02:13 +0000

    Blockit_PublicProfiles_Initial

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -22,11 +22,19 @@ require 'rspec/rails'
 #
 # Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f }
 
+require 'spec_helper'
+require 'rspec/rails'
+
+require 'factory_girl_rails'
+
 # Checks for pending migration and applies them before tests are run.
 # If you are not using ActiveRecord, you can remove this line.
+
 ActiveRecord::Migration.maintain_test_schema!
 
 RSpec.configure do |config|
+  config.include FactoryGirl::Syntax::Methods
+  
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
 
